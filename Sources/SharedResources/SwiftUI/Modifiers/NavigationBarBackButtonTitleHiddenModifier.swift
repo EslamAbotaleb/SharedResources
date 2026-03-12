6c1579f51b10ea4ddc4002b41f6475c2a3ab13ed
Eslam Baiomy <ebaiomy@youxel.com>
2026-03-12 14:55:17 +0200

    add NavigationBarBackButtonTitleHiddenModifier

---
+++ Sources/SharedResources/SwiftUI/Modifiers/NavigationBarBackButtonTitleHiddenModifier.swift
@@ -0,0 +1,43 @@
+//
+//  NavigationBarBackButtonTitleHiddenModifier.swift
+//  SharedResources
+//
+//  Created by Eslam on 12/03/2026.
+//
+
+import SwiftUI
+
+extension View {
+ public func navigationBarBackButtonTitleHidden() -> some View {
+    self.modifier(NavigationBarBackButtonTitleHiddenModifier())
+  }
+}
+
+public struct NavigationBarBackButtonTitleHiddenModifier: ViewModifier {
+
+  @Environment(\.dismiss) var dismiss
+
+  @ViewBuilder @MainActor public func body(content: Content) -> some View {
+    content
+      .navigationBarBackButtonHidden(true)
+      .navigationBarItems(
+        leading: Button(action: { dismiss() }) {
+          Image(systemName: "chevron.right")
+//                .foregroundColor(AppColors.primaryColor)
+                .font(.system(size: 20, weight: .bold))
+
+        }
+      )
+      .contentShape(Rectangle()) // Start of the gesture to dismiss the navigation
+      .gesture(
+        DragGesture(coordinateSpace: .local)
+          .onEnded { value in
+            if value.translation.width > .zero
+                && value.translation.height > -30
+                && value.translation.height < 30 {
+              dismiss()
+            }
+          }
+      )
+  }
+}
