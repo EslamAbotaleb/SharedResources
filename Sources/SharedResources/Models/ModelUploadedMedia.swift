ab1d57a1dddbae7d39063d986bbb8830011ef4a6
Eslam Baiomy <ebaiomy@youxel.com>
2026-03-13 21:49:42 +0200

    add ModelUploadedMedia

---
+++ Sources/SharedResources/Models/ModelUploadedMedia.swift
@@ -0,0 +1,91 @@
+//
+//  ModelUploadedMedia.swift
+//  SharedResources
+//
+//  Created by Eslam on 09/03/2026.
+//
+
+import Foundation
+import UIKit
+
+public struct ModelUploadedMedia: Codable, FormValue {
+
+    public var downloadUrl: String?
+    public var previewUrl: String?
+    public var viewImage: UIImage?
+    public var contentType: String?
+    public var documentType: String?
+    public var fileSize: String?
+    public var id: String?
+    public var isPublic: Bool?
+    public var name: String?
+    public var isStillUploading: Bool = false
+    public var additionalProperty01: AdditionalProperty?
+    public var additionalProperty02: AdditionalProperty?
+    public var additionalProperty03: AdditionalProperty?
+    public var additionalProperty04: AdditionalProperty?
+    
+    enum CodingKeys: String, CodingKey {
+        case contentType
+        case documentType
+        case fileSize
+        case id
+        case isPublic
+        case name
+        case additionalProperty01
+        case additionalProperty02
+        case additionalProperty03
+        case additionalProperty04
+        case downloadUrl
+        case previewUrl
+    }
+    
+    public init(downloadUrl: String? = nil,
+         previewUrl: String? = nil,
+         viewImage: UIImage? = nil,
+         contentType: String? = nil,
+         documentType: String? = nil,
+         fileSize: String? = nil,
+         id: String? = nil,
+         isPublic: Bool? = nil,
+         name: String? = nil,
+         isStillUploading: Bool = false,
+         additionalProperty01: AdditionalProperty? = nil,
+         additionalProperty02: AdditionalProperty? = nil,
+         additionalProperty03: AdditionalProperty? = nil,
+         additionalProperty04: AdditionalProperty? = nil) {
+        
+        self.downloadUrl = downloadUrl
+        self.previewUrl = previewUrl
+        self.viewImage = viewImage
+        self.contentType = contentType
+        self.documentType = documentType
+        self.fileSize = fileSize
+        self.id = id
+        self.isPublic = isPublic
+        self.name = name
+        self.isStillUploading = isStillUploading
+        self.additionalProperty01 = additionalProperty01
+        self.additionalProperty02 = additionalProperty02
+        self.additionalProperty03 = additionalProperty03
+        self.additionalProperty04 = additionalProperty04
+    }
+    
+    // Implementations for Mappable protocol
+    public init?(map: Map) {}
+    
+    public mutating func mapping(map: Map) {
+        contentType <- map["contentType"]
+        documentType <- map["documentType"]
+        fileSize <- map["fileSize"]
+        id <- map["id"]
+        isPublic <- map["isPublic"]
+        name <- map["name"]
+        additionalProperty01 <- map["additionalProperty01"]
+        additionalProperty02 <- map["additionalProperty02"]
+        additionalProperty03 <- map["additionalProperty03"]
+        additionalProperty04 <- map["additionalProperty04"]
+        downloadUrl <- map["downloadUrl"]
+        previewUrl <- map["previewUrl"]
+    }
+}
