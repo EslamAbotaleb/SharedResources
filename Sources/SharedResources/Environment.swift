ac0ad43408abe36ad6d62a281ecf4b2912c6c6eb
Eslam Baiomy <ebaiomy@youxel.com>
2026-03-14 00:36:14 +0200

    update cerqel_Environment

--- Sources/SharedResources/Environment.swift
+++ Sources/SharedResources/Environment.swift
@@ -1,5 +1,5 @@
 //
-//  cerqel_Environment.swift
+//  Environment.swift
 //  CERQEL
 //
 //  Created by iSlam on 10/6/20.
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum cerqel_Environment {
+public enum cerqel_Environment {
     // MARK: - Keys
     enum Keys {
         enum Plist {
@@ -29,12 +29,10 @@ enum cerqel_Environment {
             static let Mocking_Base_URL = "Mocking_Base_URL"
             static let appCenterKey = "appCenterKey"
             static let Search_Base_URL = "Search_Base_URL"
-            
-            static let notificationHubNamespace = "notificationHubNamespace"
-            static let notificationHubName = "notificationHubName"
-            static let notificationHubKeyName = "notificationHubKeyName"
-            static let notificationHubKey = "notificationHubKey"
+            static let bundleIdeneifier: String = "PRODUCT_BUNDLE_IDENTIFIER"
+
         }
+        
     }
     
     // Checking Environment PreDev
@@ -49,14 +47,14 @@ enum cerqel_Environment {
         }
         return dict
     }()
-    
+
     static let Api_Base_URL: String = {
         guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.API_Base_URL] as? String else {
             fatalError("API Key not set in plist for this environment")
         }
         return apiKey
     }()
-    
+        
     static let loginClientId: String = {
         guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.loginClientId] as? String else {
             fatalError("loginClientId Key not set in plist for this environment")
@@ -77,7 +75,7 @@ enum cerqel_Environment {
         }
         return apiKey
     }()
-    
+
     struct EnvironmentConfig {
         static let authConfigTokenUri: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.authConfigTokenUri] as? String else {
@@ -85,77 +83,77 @@ enum cerqel_Environment {
             }
             return apiKey
         }()
-        
+
         static let loginClientSecret: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.loginClientSecret] as? String else {
                 fatalError("loginClientSecret Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let loginIV: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.loginIV] as? String else {
                 fatalError("IV Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let encryptionKey: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.encryptionKey] as? String else {
                 fatalError("encryptionKey Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let CONTENT_Base_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.CONTENT_Base_URL] as? String else {
                 fatalError("CONTENT_Base_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let Search_Base_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Search_Base_URL] as? String else {
                 fatalError("Search_Base_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let Self_Service_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Self_Service_URL] as? String else {
                 fatalError("Self_Service_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let User_Manager_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.User_Manager_URL] as? String else {
                 fatalError("User_Manager_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let Notification_Base_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Notification_Base_URL] as? String else {
                 fatalError("Notification_Base_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let FileManager_Base_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.FileManager_Base_URL] as? String else {
                 fatalError("FileManager_Base_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let Mocking_Base_URL: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Mocking_Base_URL] as? String else {
                 fatalError("Mocking_Base_URL Key not set in plist for this environment")
             }
             return apiKey
         }()
-        
+
         static let appCenterKey: String = {
             guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.appCenterKey] as? String else {
                 fatalError("appCenterKey Key not set in plist for this environment")
@@ -163,32 +161,116 @@ enum cerqel_Environment {
             return apiKey
         }()
         
-        static let notificationHubNamespace: String = {
-            guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubNamespace] as? String else {
-                fatalError("notificationHubNamespace Key not set in plist for this environment")
-            }
-            return apiKey
-        }()
-        
-        static let notificationHubName: String = {
-            guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubName] as? String else {
-                fatalError("notificationHubName Key not set in plist for this environment")
-            }
-            return apiKey
-        }()
-        
-        static let notificationHubKeyName: String = {
-            guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubKeyName] as? String else {
-                fatalError("notificationHubKeyName Key not set in plist for this environment")
-            }
-            return apiKey
-        }()
-        
-        static let notificationHubKey: String = {
-            guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubKey] as? String else {
-                fatalError("notificationHubKey Key not set in plist for this environment")
+        static let bundleIdentifier: String = {
+            guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.bundleIdeneifier] as? String else {
+                fatalError("bundleIdentifier Key not set in plist for this environment")
             }
             return apiKey
         }()
     }
-}
+
+
+//    let authConfigTokenUri: String = {
+//        guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.authConfigTokenUri] as? String else {
+//            fatalError("authConfigTokenUri Key not set in plist for this environment")
+//
+//            let loginClientSecret: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.loginClientSecret] as? String else {
+//                    fatalError("loginClientSecret Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//            let loginIV: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.loginIV] as? String else {
+//                    fatalError("IV Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//            let encryptionKey: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.encryptionKey] as? String else {
+//                    fatalError("encryptionKey Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let CONTENT_Base_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.CONTENT_Base_URL] as? String else {
+//                    fatalError("CONTENT_Base_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let Search_Base_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Search_Base_URL] as? String else {
+//                    fatalError("Search_Base_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let Self_Service_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Self_Service_URL] as? String else {
+//                    fatalError("Self_Service_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let User_Manager_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.User_Manager_URL] as? String else {
+//                    fatalError("User_Manager_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let Notification_Base_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Notification_Base_URL] as? String else {
+//                    fatalError("Notification_Base_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let FileManager_Base_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.FileManager_Base_URL] as? String else {
+//                    fatalError("FileManager_Base_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let Mocking_Base_URL: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.Mocking_Base_URL] as? String else {
+//                    fatalError("Mocking_Base_URL Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//            let appCenterKey: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.appCenterKey] as? String else {
+//                    fatalError("appCenterKey Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//            let notificationHubNamespace: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubNamespace] as? String else {
+//                    fatalError("notificationHubNamespace Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//            let notificationHubName: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubName] as? String else {
+//                    fatalError("notificationHubName Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//            let notificationHubKeyName: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubKeyName] as? String else {
+//                    fatalError("notificationHubKeyName Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//            let notificationHubKey: String = {
+//                guard let apiKey = cerqel_Environment.infoDictionary[Keys.Plist.notificationHubKey] as? String else {
+//                    fatalError("notificationHubKey Key not set in plist for this environment")
+//                }
+//                return apiKey
+//            }()
+//
+//        }
+//      }
+    }
